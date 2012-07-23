de1196aae452214746a14ced842464938a24e490
dkrasner <daniel@math.columbia.edu>
2012-07-23 14:46:43 -0400

    Merge branch 'master' of https://github.com/brodsky/Blok
    Conflicts:
    	.half_life.py.swp

--- half_life.py
+++ half_life.py
@@ -1,41 +1,44 @@
 from datetime import datetime
 from math import floor
 
-
-def half_life(file_url):
+def half_life(file_path):
   
-  f = open(file_url, 'r')
+  f = open(file_path, 'r')
   
+  STORYID = 6 - 1 
+  TIMEKEYID = 11 - 1 
+  DELIMITER = '|'
 
   counter = 1
-  timekeys_temp = []
   ret_array = []
-
-  ### get relevant data from first line 
-  first_line = f.next()
-  storykey = first_line[6[
-  timekey = first_line[11]
+  text = f.next()
+  line = text.strip().split(DELIMITER)
+  storykey = line[STORYID]
+  timekey = line[TIMEKEYID]
+  timekeys_temp = [timekey]
+  storykey = -1
 
   #iterate over file and return storykey, half_life in hours and number of impressions
-  for line in f:
-    storykey_temp = line[6]
+  for text in f:
+    line = text.strip().split(DELIMITER)
+    storykey_temp = line[STORYID]
 
     if storykey_temp == storykey:
       counter += 1
-      timekeys_temp.append(line[11]
+      timekeys_temp.append(line[TIMEKEYID])
     else:
       hl_timekey = timekeys_temp[int(floor(counter/2))] 
       half_life =  (datetime.strptime(hl_timekey, '%Y%m%d%H') - datetime.strptime(timekey, '%Y%m%d%H')).seconds/3600
-      ret_array.append(storykey, half_life, counter)  
+      ret_array = [storykey, half_life, counter]
       
       storykey = storykey_temp
-      timekey = line[11]
+      timekey = line[TIMEKEYID]
+      timekeys_temp = [timekey]
       counter = 1
-      
       print ret_array ### not sure whether we want to make a matrix of these entries or dump to flatfile 
-
       ret_array = []
       
-    f.close()
+  f.close()
       
-
+if __name__ == '__main__':
+  half_life( '/home/ubuntu/newsright/sorted_impressions_test.csv')
