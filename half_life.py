9dde8f713353c849d3698f252351d08775385007
Brodsky <contact@kfitsolutions.com>
2012-07-23 14:36:57 +0000

    fix problems

--- half_life.py
+++ half_life.py
@@ -5,13 +5,17 @@ def half_life(file_path):
   
   f = open(file_path, 'r')
   
-  STORYID = 6 
-  TIMEKEYID = 11 
+  STORYID = 6 - 1 
+  TIMEKEYID = 11 - 1 
   DELIMITER = '|'
 
   counter = 1
-  timekeys_temp = []
   ret_array = []
+  text = f.next()
+  line = text.strip().split(DELIMITER)
+  storykey = line[STORYID]
+  timekey = line[TIMEKEYID]
+  timekeys_temp = [timekey]
   storykey = -1
 
   #iterate over file and return storykey, half_life in hours and number of impressions
@@ -19,21 +23,17 @@ def half_life(file_path):
     line = text.strip().split(DELIMITER)
     storykey_temp = line[STORYID]
 
-    if counter == 1:
-      storykey = storykey_temp
-      timekeys_temp.append(line[TIMEKEYID])
-      counter += 1
-      next
     if storykey_temp == storykey:
       counter += 1
       timekeys_temp.append(line[TIMEKEYID])
     else:
       hl_timekey = timekeys_temp[int(floor(counter/2))] 
       half_life =  (datetime.strptime(hl_timekey, '%Y%m%d%H') - datetime.strptime(timekey, '%Y%m%d%H')).seconds/3600
-      ret_array.append(storykey, half_life, counter)  
+      ret_array = [storykey, half_life, counter]
       
       storykey = storykey_temp
       timekey = line[TIMEKEYID]
+      timekeys_temp = [timekey]
       counter = 1
       print ret_array ### not sure whether we want to make a matrix of these entries or dump to flatfile 
       ret_array = []
