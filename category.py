d07c36948d08da4abfb24cbb55c93b67098afe29
Brodsky <contact@kfitsolutions.com>
2012-07-28 15:13:33 +0000

    fixed path, hidden char. bug, missing catSet.add() command in category.py

--- category.py
+++ category.py
@@ -1,14 +1,10 @@
-from datetime import datetime
-
-from math import floor
-
 import sys
 
 import csv
 
 
 
-def printCat(file_path, header):
+def printCat(file_path, file_path_out, header):
       
     f = open(file_path, 'r')
     
@@ -17,7 +13,7 @@ def printCat(file_path, header):
         head = f.next()
         
     
-    #g = open('category_run1.txt', 'w')
+    g = open(file_path_out, 'w')
 
     STORYID = 1 - 1 
     CATEGORY = 2 - 1 
@@ -41,32 +37,32 @@ def printCat(file_path, header):
     for text in f:
         tot_counter += 1
         
-        if tot_counter ==10:
-            sys.exit()
-            g.close()
-            f.close()
+        #if tot_counter ==10:
+         #   sys.exit()
+          #  g.close()
+           # f.close()
 
         line = text.strip().split(DELIMITER)
         storykey_temp = line[STORYID].strip()
         cat = line[CATEGORY].strip()
-        
           
         if storykey_temp != storykey:
-	    cat_str = '#'.join(CategorySet)
-
-            print [storykey, cat_str]
-            #csv.writer(g).writerow(printStoryInfo(storykey, timekeysList, zipcodeSet, stateSet, zipcodeflag, stateflag))
+	        #cat_str = '#'.join(CategorySet)
+            cat_str = '#'.join(CategorySet)
+            #print [storykey, cat_str]
+            csv.writer(g).writerow([storykey, cat_str])
             storykey = storykey_temp
             CategorySet = set()
-            
+        
+        CategorySet.add( cat )
                 
-    print [storykey, cat_str]
-    #csv.writer(g).writerow(printStoryInfo(storykey, timekeysList, zipcodeSet, stateSet, zipcodeflag, stateflag))
+    #print [storykey, cat_str]
+    csv.writer(g).writerow([storykey, cat_str])
       
     print 'total count: ' + str(tot_counter)    
     f.close()
     #g.close()
       
 if __name__ == '__main__':    
-    half_life('~/newsright/halflifedata/story_category_run1.csv', header=1)
+    printCat('/home/ubuntu/newsright/halflifedata/story_category_run1.csv', '/home/ubuntu/newsright/categories_run1.csv', header=1)
 
